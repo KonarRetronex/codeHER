441c9af8f1826cfb01f1c7398340534c5ac85149
Konar <38738422+KonarRetronex@users.noreply.github.com>
2025-11-06 21:14:15 +0700

    haha

---
+++ pert3/countdown_timer.py
@@ -0,0 +1,18 @@
+import time
+import pygame
+
+timer = int(input("Enter time in seconds: "))
+
+for i in range(timer, -1, -1):
+    seconds = i % 60
+    minutes = (i//60) % 60
+    hours = (i//3600) % 24
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+    timer_sound = "hidupjokowi.mp3"
+
+    if i == 0:
+        print("Time is up")
+
+
