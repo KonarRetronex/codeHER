a60f9cbc8d3cc85b1fb7d3e34e417703a18391f2
Konar <38738422+KonarRetronex@users.noreply.github.com>
2025-11-06 21:39:23 +0700

    timer working

--- pert3/countdown_timer.py
+++ pert3/countdown_timer.py
@@ -10,9 +10,17 @@ for i in range(timer, -1, -1):
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
-    timer_sound = "hidupjokowi.mp3"
+    timer_sound = "mixkit-game-show-suspense-waiting-667.wav"
 
     if i == 0:
         print("Time is up")
+        pygame.mixer.init()
+        pygame.mixer.music.load(timer_sound)
+        pygame.mixer.music.play()
+
+        time.sleep(3)
+        # while pygame.mixer.music.get_busy():
+        #     time.sleep(1)
+
 
 
